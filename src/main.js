e87069ee779b0aace10ef6a2bf433f21ae52889d
lulu-s <1192707005@qq.com>
2020-11-07 19:53:39 +0800

    add

---
+++ src/main.js
@@ -0,0 +1,23 @@
+var shared = {
+    lists: [
+        {
+            title: "文章1",
+            tag: ["标签1", "标签2"]
+        },
+        {
+            title: "文章2",
+            tag: ["标签1", "标签2"]
+        },
+        {
+            title: "文章3",
+            tag: ["标签1", "标签2"]
+        }
+    ]
+}
+
+
+
+new Vue({
+    el: '#app',
+    data: {...shared}
+})
