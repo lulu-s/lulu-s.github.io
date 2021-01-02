5b3065c7ca45b57aa32ee4f719f90305039fa5cf
lulu-s <1192707005@qq.com>
2021-01-02 20:36:07 +0800

    update bug

--- assets/lib/util.js
+++ assets/lib/util.js
@@ -18,6 +18,6 @@ document.addEventListener('touchend', function (event) {
 document.addEventListener('gesturestart', function (event) {
     event.preventDefault();
 }); 
-document.body.addEventListener('touchmove', function (e) {
-    e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
-}, {passive: false}); //passive 参数不能省略，用来兼容ios和android
+// document.body.addEventListener('touchmove', function (e) {
+//     e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
+// }, {passive: false}); //passive 参数不能省略，用来兼容ios和android
