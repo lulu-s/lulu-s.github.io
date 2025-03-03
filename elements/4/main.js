0d74d3f65977c47b7383643034448e6058e99850
lulu-s <1192707005@qq.com>
2025-03-03 20:39:07 +0800

    update

--- elements/4/main.js
+++ elements/4/main.js
@@ -2,12 +2,30 @@ let city = 101010100;
 
 // 获取 private key
 async function get_key() {
-    var res = await(await fetch(`./key.txt`)).text()
-    return res;
+    try {
+        var res = await(await fetch(`./key.txt`)).text()
+        return res;
+    } catch (error) {
+        console.log('获取key失败, 请检查key.txt文件是否存在')
+        return ''
+    }
 }
 
 // 获取实时天气
 async function get_now_weather(privateKey = '') {
-    var res = await(await fetch(`https://devapi.qweather.com/v7/weather/now?location=${city}&key=${privateKey}`)).json()
-    return res;
+    try {
+        var res = await(await fetch(`https://devapi.qweather.com/v7/weather/now?location=${city}&key=${privateKey}`)).json()
+        if(res.code === '200'){
+            return res;
+        }else{
+            console.log('获取实时天气失败, 请检查key是否正确')
+            return ''
+        }
+    } catch (error) {
+        console.log('获取实时天气失败, 请检查key是否正确')
+        return ''
+    }
 }
+
+
+
